db2bad90db6daed590eec174b182d2bf94625634
abdul-zabbar04 <a4zabbar@gmail.com>
2024-08-27 08:13:29 +0600

    csrf trusted origins added

--- library_management/settings.py
+++ library_management/settings.py
@@ -24,13 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://library-management-5ng4.onrender.com','https://*.127.0.0.1']
 
 LOGIN_URL = 'loginPage'
 
